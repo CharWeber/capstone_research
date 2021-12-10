9c3ba32a38a2546e601f02c3e087f1cecb6941e4
Charlie Weber <charlestweber@gmail.com>
2021-12-10 07:54:54 -0800

    WIP CRUD with reactfire

--- capstone-prelimb/src/components/Home.js
+++ capstone-prelimb/src/components/Home.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { getAuth } from 'firebase/auth'
 import { collection } from 'firebase/firestore';
+import {  getFirestore } from 'firebase/firestore';
 import { useFirestore, useFirestoreCollectionData} from 'reactfire';
+
 
 export default function Home() {
   const auth = getAuth();
+  const firestore = getFirestore()
   const itemsRef = collection(useFirestore(), 'items');
   const {data, status} = useFirestoreCollectionData(itemsRef);
-
   
-  function doAddItem(){
-
+  console.log(firestore)
+  const doAddItem = async () =>{
+    try{
+      await firestore.collection().set({name: new Date().getTime() + "-NAME"})
+    } catch(e){
+      alert("Error " + e.message)
+    }
   }
   return(
     <div>
