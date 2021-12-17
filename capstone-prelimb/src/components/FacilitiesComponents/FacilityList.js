b29f0ba4bfaf4ee0e60892ea49b7e5bff27c8f91
Charlie Weber <charlestweber@gmail.com>
2021-12-17 08:26:49 -0800

    add department to facility docs

--- capstone-prelimb/src/components/FacilitiesComponents/FacilityList.js
+++ capstone-prelimb/src/components/FacilitiesComponents/FacilityList.js
@@ -13,40 +13,48 @@ export default function FacilityList() {
   const { status2, data: SignInCheckResult } = useSigninCheck();
   const { data, status } = useFirestoreCollectionData(dataRef);
 
-
-  console.log(SignInCheckResult);
-  
-  const [newFacility, setNewFacility] = useState({ name: "" });
+  const [newFacility, setNewFacility] = useState({ name: "", department: "" });
   const [facilityForm, setFacilityForm] = useState(false);
-  
+
   let formVisible = null;
-  
+
   if (status2 === "loading") {
     return <div>Loading...</div>;
   } else if (SignInCheckResult === false) {
     return <div>You must be signed In to view this page</div>;
   }
 
-  
   if (facilityForm) {
     formVisible = (
       <div>
         <input
           type="text"
           placeholder="Name Of Facility"
+          id="name"
           style={{ width: "20%", marginRight: "10px" }}
           value={newFacility.name}
           onChange={(e) =>
             setNewFacility({ ...newFacility, name: e.target.value })
           }
-          />
+        />
+        <select
+          id="deaprtment"
+          name="department"
+          defaultValue='general'
+          onChange={(e) =>
+            setNewFacility({ ...newFacility, department: e.target.value })
+          }
+        >
+          <option value="athletic">Athletic</option>
+          <option value="general">General Use</option>
+        </select>
         <Button variant="contained" onClick={async () => handleNewFacility()}>
           ADD
         </Button>
       </div>
     );
   }
-  
+
   const handleNewFacility = async () => {
     try {
       await addDoc(dataRef, newFacility);
@@ -55,22 +63,18 @@ export default function FacilityList() {
     }
     setFacilityForm(false);
   };
-  
+
   return (
     <div>
-        <h3>this is where the list begins</h3>
-        <Button
-          variant="outlined"
-          onClick={() => setFacilityForm(!facilityForm)}
-        >
-          Add Facility
-        </Button>
-        {formVisible}
-        {status === "loading" ? <div>loading...</div> : null}
-        {data?.map((d) => {
-          return <Facility key={d.NO_ID_FIELD} name={d.name} />;
-        })}
-      </div>
-    );
-  }
-
+      <h3>this is where the list begins</h3>
+      <Button variant="outlined" onClick={() => setFacilityForm(!facilityForm)}>
+        Add Facility
+      </Button>
+      {formVisible}
+      {status === "loading" ? <div>loading...</div> : null}
+      {data?.map((d) => {
+        return <Facility key={d.NO_ID_FIELD} name={d.name} />;
+      })}
+    </div>
+  );
+}
